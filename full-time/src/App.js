11577c10eb4c5a1a2098157d56aa6d9943cfaaef
anshsinghal2002 <75171958+anshsinghal2002@users.noreply.github.com>
2023-02-25 16:10:34 -0500

    started bottom ribbon

--- full-time/src/App.js
+++ full-time/src/App.js
@@ -1,14 +1,13 @@
 import TopRibbon from "./components/TopRibbon.js";
+import BottomRibbon from "./components/BottomRibbon.js";
 
 function App() {
   return (
     <div className="Flex">
-       <TopRibbon>
-
-       </TopRibbon>
+      <TopRibbon></TopRibbon>
+      <BottomRibbon></BottomRibbon>
     </div>
   );
 }
 
 export default App;
- 
